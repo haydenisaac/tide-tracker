3b8c8e5185d5ecaf2cce9e4c260c7f02724142f5
Hayden Isaac <haydenisaac2906@gmail.com>
2026-09-10 21:41:38 +0100

    fix incorrect regex pattern

--- main.go
+++ main.go
@@ -69,8 +69,7 @@ func getCurrentTide(allText []string) string {
 }
 
 func isHigh(high float64, text string) bool {
-	re := regexp.MustCompile(`\d{2}\.\d{2}`)
-
+	re := regexp.MustCompile(`\d{1,2}\.\d{2}`)
 	currentHeight, err := strconv.ParseFloat(re.FindString(text), 2)
 	if err != nil {
 		panic(err)
